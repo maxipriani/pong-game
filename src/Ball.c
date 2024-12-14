8e6eddeb0c824c76fa451de4e2540c4621821641
MaxiPriani <maxipriani2002@gmail.com>
2024-12-14 18:00:24 -0300

    add Ball.c and Ball.h to implement ball logic

---
+++ src/Ball.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include "Ball.h"
+
+#define BALL_SPAWN_X (WINDOW_WIDTH / 2 - BALL_SIZE / 2)
+#define BALL_SPAWN_Y (WINDOW_HEIGHT / 2 - BALL_SIZE / 2)
+
+static void reset_ball(Ball* ball) {
+    ball->rectangle.x = BALL_SPAWN_X;
+    ball->rectangle.y = BALL_SPAWN_Y;
+    ball->vel_x *= -1;
+}
+
+void ball_logic(Ball *ball, Paddle* player, Paddle* cpu, char* score){
+
+    // Ball movement
+    ball->rectangle.x += ball->vel_x;
+    ball->rectangle.y += ball->vel_y;
+
+    // CPU Scored (horizontal collisions)
+    if (ball->rectangle.x <= 0) {
+        reset_ball(ball);
+        cpu->score++;
+        printf(score, "%d %d", player->score, cpu->score);
+    }
+    // Player scored (horizontal collisions)
+    if (ball->rectangle.x + ball->rectangle.w >= WINDOW_WIDTH) {
+        reset_ball(ball);
+        player->score++;
+        printf(score, "%d %d", player->score, cpu->score);
+    }
+
+    // Vertical collisions
+    if(ball->rectangle.y <= 0) {
+        ball->rectangle.y = 0;
+        ball->vel_y *= -1;
+    }
+    if(ball->rectangle.y + ball->rectangle.h >= WINDOW_HEIGHT) {
+        ball->rectangle.y = WINDOW_HEIGHT - ball->rectangle.h;
+        ball->vel_y *= -1;
+    }
+
+    // Collision with player's paddle
+    if (ball->rectangle.y + ball->rectangle.h >= player->rectangle.y && 
+        ball->rectangle.y <= player->rectangle.y + player->rectangle.h && 
+        ball->rectangle.x >= player->rectangle.x && 
+        ball->rectangle.x <= player->rectangle.x + player->rectangle.w) { 
+
+        ball->rectangle.x = player->rectangle.x + player->rectangle.w; 
+        ball->vel_x *= -1; 
+        
+        // Vary angle based on point of impact
+        float hit_offset_y = (player->rectangle.y + (player->rectangle.h / 2)) - (ball->rectangle.y + (ball->rectangle.h / 2));
+        float normalized_hit_offset_y = hit_offset_y / (player->rectangle.h / 2);
+        ball->vel_y = -BALL_SPEED * normalized_hit_offset_y;
+    }
+
+    // Collision with CPU's paddle
+    if(ball->rectangle.y + ball->rectangle.h >= cpu->rectangle.y && 
+       ball->rectangle.y - ball->rectangle.h <= cpu->rectangle.y + cpu->rectangle.h && 
+       ball->rectangle.x + ball->rectangle.w >= cpu->rectangle.x && 
+       ball->rectangle.x <= cpu->rectangle.x + cpu->rectangle.w) {
+        
+        ball->rectangle.x = cpu->rectangle.x - ball->rectangle.w;
+        ball->vel_x *= -1;
+        
+        // Vary angle based on point of impact
+        float hit_offset_y = (cpu->rectangle.y + (cpu->rectangle.h / 2)) - (ball->rectangle.y + (ball->rectangle.h / 2));
+        float normalized_hit_offset_y = hit_offset_y / (cpu->rectangle.h / 2);
+        ball->vel_y = -BALL_SPEED * normalized_hit_offset_y;
+    }    
+
+}
---
+++ src/Ball.h
@@ -0,0 +1,23 @@
+#ifndef BALL_H_
+#define BALL_H_
+
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+#define BALL_SIZE 15
+#define BALL_SPEED 7
+#define BALL_SPAWN_X ((WINDOW_WIDTH / 2) - (BALL_SIZE / 2))
+#define BALL_SPAWN_Y ((WINDOW_HEIGHT / 2) - (BALL_SIZE / 2))
+
+typedef struct {
+    SDL_Rect rectangle;
+    float vel_x;
+    float vel_y;
+} Ball;
+
+#include "Paddle.h"
+
+void ball_logic(Ball *ball, Paddle *player, Paddle *cpu, char *score_str);
+
+#endif 
