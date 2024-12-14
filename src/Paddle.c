75a682488c300c114d85f989013dd62799c897b3
MaxiPriani <maxipriani2002@gmail.com>
2024-12-14 18:03:04 -0300

    add Paddle.c and Paddle.h to implement paddle logic

---
+++ src/Paddle.c
@@ -0,0 +1,44 @@
+#include "Ball.h"
+#include "Paddle.h"
+
+void constrain_paddle_position(Paddle *paddle) {
+    if (paddle->rectangle.y <= 0) {
+        paddle->rectangle.y = 0;
+    }
+    if (paddle->rectangle.y + paddle->rectangle.h >= WINDOW_HEIGHT) {
+        paddle->rectangle.y = WINDOW_HEIGHT - paddle->rectangle.h;
+    }
+}
+
+void cpu_logic(Paddle *cpu, Ball *ball) {
+    const int SCREEN_CENTER = WINDOW_HEIGHT / 2;
+
+    if (ball->rectangle.x > CPU_ACTIVATION_ZONE) {
+        if (ball->rectangle.y >= cpu->rectangle.y) {
+            cpu->rectangle.y += PADDLE_SPEED;
+        } else if (ball->rectangle.y <= cpu->rectangle.y + cpu->rectangle.h) {
+            cpu->rectangle.y -= PADDLE_SPEED;
+        }
+    } else {
+        float paddle_center = cpu->rectangle.y + (cpu->rectangle.h / 2);
+
+        if (paddle_center > SCREEN_CENTER + CPU_REST_MARGIN) {
+            cpu->rectangle.y -= PADDLE_SPEED;
+        } else if (paddle_center < SCREEN_CENTER - CPU_REST_MARGIN) {
+            cpu->rectangle.y += PADDLE_SPEED;
+        }
+    }
+
+    constrain_paddle_position(cpu);
+}
+
+void player_logic(Paddle *player) {
+    if (player->controls[0] && !player->controls[1]) {
+        player->rectangle.y -= PADDLE_SPEED;
+    }
+    if (player->controls[1] && !player->controls[0]) {
+        player->rectangle.y += PADDLE_SPEED;
+    }
+
+    constrain_paddle_position(player);
+}
---
+++ src/Paddle.h
@@ -0,0 +1,22 @@
+#ifndef PADDLE_H_
+#define PADDLE_H_
+
+#include <SDL2/SDL.h>
+#include "Ball.h"
+
+#define PADDLE_SPEED 10
+#define CPU_ACTIVATION_ZONE (WINDOW_WIDTH - WINDOW_WIDTH / 4)
+#define CPU_REST_MARGIN 10
+
+typedef struct {
+    SDL_Rect rectangle; 
+    int score;          
+    int controls[2];    
+} Paddle;
+
+void cpu_logic(Paddle *cpu, Ball *ball);
+void player_logic(Paddle *player);
+
+void constrain_paddle_position(Paddle *paddle);
+
+#endif 
